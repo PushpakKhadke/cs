d722123652543ead59be2999a41f0bb9cd65943a
Pushpak Khadke <100704509+PushpakKhadke@users.noreply.github.com>
2023-08-13 12:46:30 +0530

    Add files via upload

---
+++ C/cd/two.c
@@ -0,0 +1,25 @@
+// Hello name program using user input
+
+//#include<stdio.h>
+//
+//int main(){	
+//	char answer[500];
+//	printf("What's your name? ");
+//	scanf("%499s",&answer);
+//	printf("Hello,%s!\n",answer);
+//	return 0;	
+//}
+
+#include<stdio.h>
+
+int main(){	
+	char lp[500] = "po";
+	if (lp == "po"){
+		printf("lo");
+	}
+	else{
+		break;
+	}
+	return 0;	
+}
+
